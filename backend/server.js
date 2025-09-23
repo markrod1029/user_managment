3cefb4edfdd0152a2d1d76c0b4c93452b448f6ed
markrod1029 <103241234+markrod1029@users.noreply.github.com>
2025-09-23 12:48:08 +0800

    production

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,7 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).json({ error: 'Route not found' });
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
